8b82031abf3026721ce98013c2e51f4129933084
Amir Hedayati <a3hedaya@uwaterloo.ca>
2018-09-21 12:00:28 -0400

    Simplified the script

--- counter.js
+++ counter.js
@@ -1,21 +1,6 @@
 var highlight=window.getSelection().toString();
 var i =0;
-var count = 1;
-for (i = 0; i < highlight.length; i++){
-  if (highlight.substring(i,i+1).localeCompare(" ")==0){
-    count++;
-  }
 
-}
-if (highlight.substring(highlight.length-1).localeCompare(" ")==0){
-  count--;
-}
-
-if (highlight.substring(0,1).localeCompare(" ")==0){
-  count--;
-}
-
-if(highlight.length == 0){
-  count = 0;
-}
+var seperated = highlight.split(" ");
+var count = seperated.length;
 alert("Word count: "+ count+"\n" +"Character count: "+highlight.length+"\n");
